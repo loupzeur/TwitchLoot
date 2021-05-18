458c5d1d76f053bb353ec347f28bd890b6ac0c61
noid <noid@noid.com>
2021-05-18 23:09:08 +0200

    new style

--- looter.js
+++ looter.js
@@ -1,7 +1,12 @@
 function clickedstuff(){
-    var t = document.querySelectorAll('.tw-button .tw-button--success, button[class^=ScCoreButton]');
-    if(t.length>0){
+    var t = document.querySelectorAll('.tw-button .tw-button--success');
+    if(t.length>0){//old one
         t[0].click();
+        chrome.runtime.sendMessage({isLooted:true})
+    }
+    t = document.querySelectorAll('.community-points-summary button[class^=ScCoreButton]')
+    if(t.length>0){//old new one
+        t[1].click();
         chrome.runtime.sendMessage({isLooted:true})
     }
     setTimeout(clickedstuff,5000);
