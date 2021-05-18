9f133fd2607014af26a749b5e96e413859ae4065
noid <noid@noid.com>
2021-05-18 23:10:40 +0200

    has to be >1 ^^

--- looter.js
+++ looter.js
@@ -5,7 +5,7 @@ function clickedstuff(){
         chrome.runtime.sendMessage({isLooted:true})
     }
     t = document.querySelectorAll('.community-points-summary button[class^=ScCoreButton]')
-    if(t.length>0){//old new one
+    if(t.length>1){//old new one
         t[1].click();
         chrome.runtime.sendMessage({isLooted:true})
     }
