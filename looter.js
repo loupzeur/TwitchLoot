e572e549edc64ea7f96fc164bf5130b24544e6d5
loup <loup@laptop>
2021-08-17 22:59:10 +0200

    fix some stuff

--- looter.js
+++ looter.js
@@ -1,13 +1,21 @@
 function clickedstuff(){
     var t = document.querySelectorAll('.tw-button .tw-button--success');
+    console.log(t);
     if(t.length>0){//old one
         t[0].click();
-        chrome.runtime.sendMessage({isLooted:true})
+        //chrome.runtime.sendMessage({isLooted:true})
+    }
+    t = document.querySelectorAll('button[aria-label$="onus"]');
+    console.log(t);
+    if(t.length>0){//old one
+        t[0].click();
+        //chrome.runtime.sendMessage({isLooted:true})
     }
     t = document.querySelectorAll('.community-points-summary button[class^=ScCoreButton]')
+    console.log(t);
     if(t.length>1){//old new one
         t[1].click();
-        chrome.runtime.sendMessage({isLooted:true})
+        //chrome.runtime.sendMessage({isLooted:true})
     }
     setTimeout(clickedstuff,5000);
 }
